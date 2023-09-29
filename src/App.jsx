6a0cce32f36c683a62a3266577ab251884a122a5
DevAro178 <me.ammad1786@gmail.com>
2023-09-29 21:07:30 +0500

    setup SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <>How are you!!!</>;
+  return (
+    <div className="container">
+      <div className="controls">controls will be here.</div>
+      <div className="main">this is the main grid</div>
+      <div className="winsDisplay">here will be the previous score</div>
+    </div>
+  );
 }
 
 export default App;
