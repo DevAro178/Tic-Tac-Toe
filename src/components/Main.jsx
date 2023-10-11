e2b675e6b77c0b1f74eaad38585c415cd904db4e
DevAro178 <me.ammad1786@gmail.com>
2023-10-11 21:48:38 +0500

    Player class added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,15 @@
 import Tile from "./Tile";
+import Player from "../assets/logic/player";
 
 const Main = () => {
+  const p1 = new Player(0);
+  p1.setWins(2);
+  p1.setLosses(5);
+  p1.setDraws(1);
+  console.log(p1.getMarker());
+  console.log(p1.getWins());
+  console.log(p1.getLosses());
+  console.log(p1.getDraws());
   return (
     <div className="main">
       <Tile />
